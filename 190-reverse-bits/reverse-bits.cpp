ad504b51d8fe6cbb0f2654b5f624e98344fdd463
Jay Kumar <jay687848@gmail.com>
2026-06-21 11:48:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (45.52%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -3,8 +3,7 @@ public:
     int reverseBits(int n) {
         int ans=0;
         for (int i=0; i<32; i++){
-            if ((n&1)==0) ans=ans<<1;
-            else ans=(ans<<1)+1;
+            ans=(ans<<1)|(n&1);
             n=n>>1;
         }
         return ans;
