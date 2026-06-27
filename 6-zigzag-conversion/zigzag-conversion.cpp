29059798dcef0e5021bd388690444a905f8a010a
Jay Kumar <jay687848@gmail.com>
2026-06-27 11:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (90.95%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string convert(string s, int numRows) {
         if (s.length()<=2 || numRows==1) return s;
-        int index1=2*numRows-3;
+        int index1=2*numRows-2;
         int index2=0;
         string ans;
         for (int i=0; i<numRows; i++){
@@ -11,17 +11,16 @@ public:
                 
                 if (index1!=0 && j<s.size()){
                     ans.push_back(s[j]);
-                    j=j+index1+1;
+                    j=j+index1;
                 }
                 if (index2!=0 && j<s.size()){
                     ans.push_back(s[j]);
-                    j=j+index2+1;
+                    j=j+index2;
                 }
             }
-            if (index1==1) index1=0;
-            else index1-=2;
-            if (index2==0) index2=1;
-            else index2+=2;
+
+            index1-=2;
+            index2+=2;
             
         }
         return ans;
