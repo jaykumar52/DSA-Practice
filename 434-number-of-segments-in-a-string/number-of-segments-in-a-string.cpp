b25bffb1e535a54d2dd4d464df2b8e6f31463e2a
Jay Kumar <jay687848@gmail.com>
2026-07-13 12:40:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (18.41%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -2,18 +2,10 @@ class Solution {
 public:
     int countSegments(string s) {
         int count =0;
-        int i=0;
-        bool flag = false;
-        while (i<s.size()){
-            while (i<s.size() && s[i]==' ') i++;
-            while (i<s.size() && s[i]!=' '){
-                i++;
-                flag=true;
-            }
-            if (flag){
-                flag=false;
-                count++;
-            }
+        string word;
+        stringstream ss(s);
+        while (ss>>word){
+            count++;
         }
         return count;
     }
