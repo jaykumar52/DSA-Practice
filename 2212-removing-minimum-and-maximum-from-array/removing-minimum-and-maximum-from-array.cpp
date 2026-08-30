7fd49e0c075a07fe1fcd8c0f2d752396ed659da1
Jay Kumar <jay687848@gmail.com>
2026-08-30 20:43:08 +0530

    Time: 4 ms (24.32%) | Memory: 92.1 MB (58.82%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -4,13 +4,12 @@ public:
         int minnum=0;
         int maxnum=0;
         for (int i=0; i<nums.size(); i++){
-            if (nums[i]>nums[maxnum]){
-                maxnum=i;
-            }
-            if (nums[i]<nums[minnum]){
-                minnum=i;
-            }
+            if (nums[i]>nums[maxnum]) maxnum=i;
+            if (nums[i]<nums[minnum]) minnum=i;
         }
-        return min(min(max(minnum, maxnum)+1, (int)nums.size()-min(minnum, maxnum)), min(minnum, maxnum)+1+(int)nums.size()-max(minnum, maxnum));
+        int left = min(minnum, maxnum);
+        int right = max(minnum, maxnum);
+        int n=nums.size();
+        return min(min(right+1, n-left), left+1+n-right);
     }
 };
