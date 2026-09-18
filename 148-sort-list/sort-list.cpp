c52b3d6cac0e829f8a68804f5684ebdc020987a6
Jay Kumar <jay687848@gmail.com>
2026-09-18 22:40:45 +0530

    Time: 67 ms (5.98%) | Memory: 86.3 MB (5.01%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -9,19 +9,46 @@
  * };
  */
 class Solution {
+private:
+    ListNode* getMid(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* merge(ListNode* list1, ListNode* list2){
+        ListNode* dummy=new ListNode(0);
+        ListNode* tail=dummy;
+        while (list1 && list2){
+            if (list1->val>list2->val){
+                tail->next=list2;
+                list2=list2->next;
+                tail=tail->next;
+            } else{
+                tail->next=list1;
+                list1=list1->next;
+                tail=tail->next;
+            }
+        }
+        if (list1) tail->next=list1;
+        else if(list2) tail->next=list2;
+        ListNode* merged=dummy->next;
+        delete dummy;
+        return merged;
+    }
 public:
     ListNode* sortList(ListNode* head) {
-        multiset<int> ms;
-        ListNode* temp=head;
-        while (temp!=nullptr){
-            ms.insert(temp->val);
-            temp=temp->next;
-        }
-        temp=head;
-        for (int num : ms){
-            temp->val=num;
-            temp=temp->next;
-        }
-        return head;
+        if (head==nullptr || head->next==nullptr) return head;
+        ListNode* mid = getMid(head);
+        ListNode* rightHead=mid->next;
+        mid->next=nullptr;
+
+        ListNode* left=sortList(head);
+        ListNode* right=sortList(rightHead);
+
+        return merge(left, right);
     }
 };
